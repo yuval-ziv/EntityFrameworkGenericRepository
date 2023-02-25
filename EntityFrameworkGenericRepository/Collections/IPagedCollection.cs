d0110fd7c0f7ebbadba0de63e84a4951c3b1de83
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2023-02-25 03:00:12 +0200

    Added documentation to IPagedCollection.cs and PagedCollection.cs.

--- EntityFrameworkGenericRepository/Collections/IPagedCollection.cs
+++ EntityFrameworkGenericRepository/Collections/IPagedCollection.cs
@@ -1,6 +1,13 @@
 ﻿namespace EntityFrameworkGenericRepository.Collections;
 
+/// <summary>
+/// Defines methods to manipulate generic paged collections.
+/// </summary>
+/// <inheritdoc cref="ICollection{T}"/>
 public interface IPagedCollection<T> : ICollection<T>
 {
+    /// <summary>
+    /// The total amount of results the paged collection is taken from.
+    /// </summary>
     public int TotalAmount { get; }
 }
--- EntityFrameworkGenericRepository/Collections/PagedCollection.cs
+++ EntityFrameworkGenericRepository/Collections/PagedCollection.cs
@@ -2,15 +2,28 @@
 
 namespace EntityFrameworkGenericRepository.Collections;
 
+/// <summary>
+/// Provides the base class for a generic paged collection.
+/// </summary>
+/// <inheritdoc cref="Collection{T}"/>
 public class PagedCollection<T> : Collection<T>, IPagedCollection<T>
 {
     public int TotalAmount { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PagedCollection{T}"/> class as a wrapper for the specified list.
+    /// </summary>
     public PagedCollection()
     {
         TotalAmount = 0;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PagedCollection{T}"/> class as a wrapper for the specified list.
+    /// </summary>
+    /// <param name="list">the list that is wrapped by the new collection.</param>
+    /// <param name="totalAmount">the total amount of results the paged collection is taken from.</param>
+    /// <exception cref="ArgumentNullException">list is null.</exception>
     public PagedCollection(IList<T> list, int totalAmount) : base(list)
     {
         TotalAmount = totalAmount;
