0ad1dd0fa0141877c11a63e4274df99972b0e59a
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2023-04-13 20:42:09 +0300

    fixed IEnumerableExtensionMethods.cs IsNullOrEmpty missing !

--- EntityFrameworkGenericRepository/Utils/ExtensionMethods/IEnumerableExtensionMethods.cs
+++ EntityFrameworkGenericRepository/Utils/ExtensionMethods/IEnumerableExtensionMethods.cs
@@ -4,6 +4,6 @@ public static class IEnumerableExtensionMethods
 {
     public static bool IsNullOrEmpty<T>(this IEnumerable<T>? collection)
     {
-        return collection == null || collection.Any();
+        return collection == null || !collection.Any();
     }
 }
